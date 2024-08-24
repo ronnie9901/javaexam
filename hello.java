6ac50c27a1540b987948041bc901653ecba20a3d
Rohan nayak <148763509+ronnie9901@users.noreply.github.com>
2024-08-24 19:01:04 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,27 +1,36 @@
+
+Conversation opened. 1 unread message.
+
+Skip to content
+Using Gmail with screen readers
+1 of 2,631
+(no subject)
+Inbox
+
+Sahil Kashyap <rk4852115@gmail.com>
+18:56 (3 minutes ago)
+to 5940, me
+
 import java.util.Scanner;
 
-public class hello {
-
-    public static void main(String[] args)
-    {
-
-        int a ;
+public class SumOfEven {
+    public static void main(String[] args) {
         int sum = 0;
-        Scanner  input =  new  Scanner(System.in);
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value: ");
+        n = sc.nextInt();
+        int user = n;
+        sumOfEven(n, sum, user);
+    }
+    static void sumOfEven(int n, int sum, int user) {
 
-        System.out.println(" enter the a");
-        a = input.nextInt();
-
-            for (int i=1;i<a;i++){
-
-                if(i%2==0)
-                {
-                    System.out.println(i);
-                }
-
-            }
-
+        if(n > 0){
+            sum = sum + n;
+            n--;
+            sumOfEven(n, sum, user);
+        } else {
+            System.out.println("Sum of even numbers between 1 to "+user +": " + sum);
+        }
     }
 }
-
-
